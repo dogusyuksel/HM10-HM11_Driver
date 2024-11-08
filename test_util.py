e5757f13a987fde9e135ce6234a6d918ceace1cd
dogusyuksel <dogusyuk@gmail.com>
2024-11-08 21:32:52 +0000

    Update test_util.py

--- test_util.py
+++ test_util.py
@@ -9,11 +9,11 @@ def test_arg_check_filename():
         description='test desc',
         epilog='test epilog')
     parser.add_argument('-d', '--verbose',
-                    action='store_true',
-                    help='verbosity setting')
+        action='store_true',
+        help='verbosity setting')
     parser.add_argument('-i', '--interface',
-                    action='store',
-                    help='serial interface name')
+        action='store',
+        help='serial interface name')
     args = parser.parse_args(['--verbose'])
     u = utils.Utils(args)
 
